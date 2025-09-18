272b86ee929ab885283e79d9789e4e540ad8f47d
Matheus <matheusrike.alves@gmail.com>
2025-09-18 15:22:58 -0300

    chore: add eslint and prettier basic config

---
+++ backend/dist/index.js
@@ -0,0 +1,10 @@
+import { fastify } from 'fastify';
+const app = fastify();
+const PORT = 3000;
+app.listen({ port: 3000 }, (err) => {
+    console.log(`Server running on http://localhost:${PORT}`);
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+});
