1acd7d72eba0fb06601cfee357d5360d45e607ca
Matheus <matheusrike.alves@gmail.com>
2025-09-19 15:00:22 -0300

    chore: restructure backend codebase, move entry point to src/server.ts and add prisma client

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,10 @@
 import { fastify } from 'fastify';
+import prisma from '../prisma/client.ts';
 
 const app = fastify();
 const PORT = 3000;
+
+console.log(await prisma.user.findMany());
 
 app.listen({ port: 3000 }, (err) => {
 	console.log(`Server running on http://localhost:${PORT}`);
