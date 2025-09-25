bd057206436acbbf3352f64f5167c6af6c887c52
Matheus Alves <matheusrike.alves@gmail.com>
2025-09-25 17:21:58 -0300

    feat: add Swagger documentation and update Fastify server configuration to improve API documentation and type safety

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,25 @@
 import { configDotenv } from 'dotenv';
 import Fastify from 'fastify';
-import type { FastifyInstance } from 'fastify';
+import type {} from 'fastify-type-provider-zod';
+import {
+	validatorCompiler,
+	serializerCompiler,
+	type ZodTypeProvider,
+	jsonSchemaTransform,
+} from 'fastify-type-provider-zod';
 import cookie from '@fastify/cookie';
 import jwt from '@fastify/jwt';
 import authorizationPlugin from './plugins/authorization.plugin.ts';
+import { fastifySwagger } from '@fastify/swagger';
+import { fastifySwaggerUi } from '@fastify/swagger-ui';
 
 // Basic configs
 configDotenv({ quiet: true });
-const app: FastifyInstance = Fastify();
+const app = Fastify().withTypeProvider<ZodTypeProvider>();
 const port: number = parseInt(process.env.PORT || '3000', 10);
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
 // Plugins registration
 app.register(cookie, {
@@ -18,6 +29,21 @@ app.register(jwt, {
 	secret: process.env.JWT_SECRET!,
 });
 app.register(authorizationPlugin);
+
+// Documentation plugins registration
+app.register(fastifySwagger, {
+	openapi: {
+		info: {
+			title: 'Twent API',
+			version: '1.0.0',
+			description: 'Twent API documentation',
+		},
+	},
+	transform: jsonSchemaTransform,
+});
+app.register(fastifySwaggerUi, {
+	routePrefix: '/docs',
+});
 
 // Server start
 app.listen({ port: port }, (err) => {
--- backend/src/types/types.ts
+++ backend/src/types/types.ts
@@ -1,3 +1,20 @@
+import {
+	FastifyInstance,
+	RawServerDefault,
+	RawRequestDefaultExpression,
+	RawReplyDefaultExpression,
+	FastifyBaseLogger,
+} from 'fastify';
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
+
+export type fastifyTypedInstance = FastifyInstance<
+	RawServerDefault,
+	RawRequestDefaultExpression,
+	RawReplyDefaultExpression,
+	FastifyBaseLogger,
+	ZodTypeProvider
+>;
+
 export interface IApiResponseOptions<T, E> {
 	statusCode: number;
 	success: boolean;
