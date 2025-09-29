3b5160594a522ebe1a8580b809b9721d7e8b5fce
GustavoTorelli <gustavo.torelli17@gmail.com>
2025-09-29 17:20:36 -0300

    frontend: update home components

--- frontend/src/components/web/pages/Home/Features/mainContainer.tsx
+++ frontend/src/components/web/pages/Home/Features/mainContainer.tsx
@@ -21,7 +21,10 @@ const MainContainer = () => {
               </p>
 
               {/* Image Mobile */}
-              <div className="md:hidden relative aspect-square w-full bg-slate-100 rounded-2xl overflow-hidden mb-6"></div>
+              {/* Image Desktop */}
+              <div className="block md:hidden justify-center items-center relative aspect-square w-full bg-slate-100 rounded-2xl overflow-hidden  transition-transform duration-300  shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">
+                <MainImage />
+              </div>
 
               {/* Button */}
               <Link href="/collection">
--- frontend/src/components/web/pages/Home/hero/hero.tsx
+++ frontend/src/components/web/pages/Home/hero/hero.tsx
@@ -8,24 +8,24 @@ const Hero = () => {
   return (
     <div
       className="
-        relative w-full min-h-screen flex items-end
+        relative w-full h-screen 
+        flex items-end md:items-center
         bg-[url('/img/home/bannerMobile.webp')] 
         md:bg-[url('/img/home/banner.png')] 
         bg-cover bg-center bg-no-repeat
       "
     >
-      {/*blur*/}
-      <div className="absolute inset-0 " />
 
-      {/* button and text */}
-      <div className="relative z-10 p-6 md:p-12 max-w-lg">
-        <h1 className="text-white text-3xl md:text-6xl font-bold mb-4">
-          Explore a excelência
+      <div className="absolute inset-0 md:bg-black/10" />
+
+      <div className="relative z-10 p-6 md:px-12 max-w-lg md:max-w-2xl">
+        <h1 className="text-white text-3xl md:text-8xl font-bold mb-4 md:uppercase md:leading-tight md:tracking-tight">
+          Explore a<br className="hidden md:block" />excelência
         </h1>
-        <p className="text-white/80 text-lg md:text-3xl mb-6">
+        <p className="text-white/80 md:text-white/90 text-lg md:text-base mb-6 md:mb-8 md:max-w-md">
           Descubra nossa coleção exclusiva de relógios de luxo.
         </p>
-        <Button variant="standartButtonDark" size="standartButton" className="dark" >
+        <Button variant="standartButtonDark" size="standartButton" className="dark">
           <Link href="/collection">
             Conheça mais
           </Link>
