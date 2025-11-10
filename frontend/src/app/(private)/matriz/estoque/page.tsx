c46e7fc9b750170f4e2a04f9db99d82f97512052
ToledoP <lucas.toledo05082007@gmail.com>
2025-11-10 16:02:04 -0300

    frontend: Creating and placing the first items on the inventory page.

--- frontend/src/app/(private)/matriz/estoque/page.tsx
+++ frontend/src/app/(private)/matriz/estoque/page.tsx
@@ -1,5 +1,12 @@
-export default function Estoque(){
-    return(
-        <></>
+import StockCards from "@/components/private/views/stock/stock-cards";
+import EstoqueHeader from "@/components/private/views/stock/stock-header";
+
+export default function Estoque() {
+    return (
+        <section className="flex flex-col gap-5  ">
+            <EstoqueHeader />
+            <StockCards />
+        </section>
+        
     )
 }
---
+++ frontend/src/components/private/views/stock/stock-header.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Camera, Calendar, Mail, MapPin } from "lucide-react";
+
+export default function EstoqueHeader() {
+  return (
+    <Card>
+      <CardContent className="p-6">
+        
+      </CardContent>
+    </Card>
+  );
+}
