b7770a840d6fdc46a6de8845daa5ea1cdc3b6556
GustavoTorelli <gustavo.torelli17@gmail.com>
2025-12-09 13:57:51 -0300

    Refactor Home component for improved code readability and formatting

--- frontend/src/app/(web)/page.tsx
+++ frontend/src/app/(web)/page.tsx
@@ -1,20 +1,18 @@
-import Hero from "@/components/web/views/Home/hero/hero"
-import MainContainer from "@/components/web/views/Home/Features/mainContainer"
-import AboutContainer from "@/components/web/views/Home/Features/aboutContainer"
-import TrustSection from "@/components/web/views/Home/Features/trustContainer"
-
-
+import Hero from "@/components/web/views/Home/hero/hero";
+import MainContainer from "@/components/web/views/Home/Features/mainContainer";
+import AboutContainer from "@/components/web/views/Home/Features/aboutContainer";
+import TrustSection from "@/components/web/views/Home/Features/trustContainer";
 
 export default function Home() {
   return (
     <>
       <div className=" flex flex-col h-auto justify-center items-center">
         <Hero />
-     
+
         <MainContainer />
         <AboutContainer />
-    <TrustSection/>
+        <TrustSection />
       </div>
     </>
-  )
+  );
 }
